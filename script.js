c42c365378894200b86d2e895ba56af77f233f84
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2023-10-19 20:26:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+// Function to lock the screen orientation
+function lockScreenOrientation() {
+    if (screen.orientation && screen.orientation.lock) {
+        screen.orientation.lock('portrait-primary')
+            .then(function() {
+                console.log('Screen orientation locked successfully');
+            })
+            .catch(function(error) {
+                console.error('Failed to lock screen orientation: ' + error);
+            });
+    }
+}
+
+// Call the function to lock the screen orientation when the app loads
+lockScreenOrientation();
