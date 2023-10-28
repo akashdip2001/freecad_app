89bde8a5d3283500d37a32d38f16e8f62ba576f5
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2023-10-29 03:11:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,55 @@ function redirectToWebsite() {
     // Replace 'your-website-url' with your actual website URL
     window.location.href = 'https://akashdip2001.github.io/freecad_app/';
 }
+
+
+// Styles for the image slider 
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const indicatorsContainer = document.querySelector('.indicators');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let currentIndex = 0;
+
+// Add indicators
+slides.forEach((_, index) => {
+    const indicator = document.createElement('div');
+    indicator.classList.add('indicator');
+    indicator.addEventListener('click', () => goToSlide(index));
+    indicatorsContainer.appendChild(indicator);
+});
+
+const indicators = document.querySelectorAll('.indicator');
+
+function updateIndicators() {
+    indicators.forEach((indicator, index) => {
+        if (index === currentIndex) {
+            indicator.classList.add('active');
+        } else {
+            indicator.classList.remove('active');
+        }
+    });
+}
+
+function goToSlide(index) {
+    currentIndex = index;
+    const translateX = -currentIndex * 100 + '%';
+    slider.style.transform = `translateX(${translateX})`;
+    updateIndicators();
+}
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        goToSlide(currentIndex - 1);
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < slides.length - 1) {
+        goToSlide(currentIndex + 1);
+    }
+});
+
+goToSlide(currentIndex);
