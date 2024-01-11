2dffeb2069c996068de5ccc918cadeadef41b54f
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2024-01-11 17:48:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,4 +49,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Initialize the slider
     goToSlide(currentIndex);
+
+
+    
+     // Function to check if the user is on a desktop device
+        function isDesktop() {
+            return window.innerWidth > 768; // Adjust the width based on your design
+        }
+
+        // Function to redirect to another page
+        function redirectToAnotherPage() {
+            window.location.href = '404.html';
+        }
+
+        // Check if the user is on a desktop and redirect if true
+        window.addEventListener('DOMContentLoaded', function () {
+            if (isDesktop()) {
+                redirectToAnotherPage();
+            }
+        });
+     
+
+      // Function to disable right-click context menu
+        function disableRightClick(event) {
+            event.preventDefault();
+        }
+
+        // Attach the disableRightClick function to the contextmenu event
+        window.addEventListener('contextmenu', disableRightClick);
 });
